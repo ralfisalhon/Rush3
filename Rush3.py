ca750bc477bc62610f89fc90e81d2adfb4af047a
ralfisalhon <ralfi.salhon@ggl.hisarschool.k12.tr>
2016-05-11 22:22:37 +0300

    v4
    added info and hover over cards

--- Rush3.py
+++ Rush3.py
@@ -5,31 +5,38 @@ from random import randrange
 pygame.init()
 pygame.display.set_caption("Rush3")
 
+##########
+updates = False
+##########
+
 castle =  pygame.image.load("castle.png")
 coin =  pygame.image.load("coin.png")
 sword =  pygame.image.load("sword.png")
 
-red = (255,0,0)
-green = (0,255,0)
-blue = (0,0,255)
-darkBlue = (0,0,128)
-white = (255,255,255)
-black = (0,0,0)
-pink = (255,200,200)
-cyan = (0,255,255)
-yellow = (255,255,0)
-brown = (139,69,19)
-orange = (255,165,0)
+isimler = ["Ralfi", "Riva", "Semih"]
+
+#KEY: 		 R  G  B
+red = 		(255,0,0)
+green = 	(0,255,0)
+blue = 		(0,0,255)
+darkBlue =	(0,0,128)
+white = 	(255,255,255)
+black = 	(0,0,0)
+pink = 		(255,200,200)
+cyan = 		(0,255,255)
+yellow = 	(255,255,0)
+brown = 	(139,69,19)
+orange = 	(255,165,0)
 
 Width = 955
-Height = 620
+Height = 640
 screen = pygame.display.set_mode((Width,Height))
 back = pygame.Surface((Width,Height))
 background = back.convert()
 background.fill(orange)
 screen.blit(background,(0,0))
 
-font = pygame.font.Font(None, 46)
+font = pygame.font.Font(None, 18)
 text = font.render("", 1, black)
 textpos = text.get_rect()
 
@@ -60,8 +67,8 @@ array.append(["NONE", 8, "TC", [0, 0], (3), (440,140), green])
 array.append(["NONE", 9, "CT", [0, 0], (14), (370,380), brown])
 array.append(["NONE", 10, "AT", [0, 0], (6, 14), (370,500), brown])
 array.append(["NONE", 11, "TA", [0, 0], (7, 15), (600,80), green])
-array.append(["NONE", 12, "C", [0], (16), (640,200), pink])
-array.append(["NONE", 13, "C", [0], (16), (640,320), pink])
+array.append(["NONE", 12, "C", [0], (16), (630,200), pink])
+array.append(["NONE", 13, "C", [0], (16), (630,320), pink])
 array.append(["NONE", 14, "ACT", [0, 0, 0], (9, 10, 17), (530,440), brown])
 array.append(["NONE", 15, "AT", [0, 0], (11, 16), (760,140), pink])
 array.append(["RED", 16, "ACT", [0, 0, 0], (12, 13, 15, 17), (725,260), pink])
@@ -69,8 +76,7 @@ array.append(["NONE", 17, "AT", [0, 0], (14, 16), (760,380), pink])
 
 #Prints the information for group 'number'
 def getInfo(number):
-	print "OWNER:", array[number][0], "  NUMBER:", array[number][1], "  TYPE:", array[number][2], "  TYPES INCLUDE:", array[number][3], "  ADJACENT TO:", array[number][4]
-	print
+	return "OWNER:", array[number][OWNER], "  NUMBER:", array[number][NUM], "  TYPE:", array[number][TYPE], "  CARDS:", array[number][CARDS], "  ADJACENT TO:", array[number][ADJACENT], "  LOCATION:", array[number][LOCATION], "  COLOUR:", array[number][COLOUR]
 
 #Changes the owner of group 'number' to a new color.
 def changeOwner(number, new):
@@ -97,9 +103,9 @@ def DrawLines():
 				startloc = (array[item][LOCATION][x]+35,array[item][LOCATION][y]+50)
 				endloc = (array[end][LOCATION][x]+35,array[end][LOCATION][y]+50)
 				pygame.draw.line(screen, black, startloc, endloc, 15)
-				pygame.display.update()
-		except: print "Problem while printing line", item
-
+				if(updates): pygame.display.update()
+		except: #print "Problem while printing line", item
+			pass
 
 #Draws screen for next update
 def Draw():
@@ -108,7 +114,6 @@ def Draw():
 	DrawLines()
 
 	for item in range(len(array)):
-
 		add = 0
 		current = 0
 		for count in range(len(array[item][TYPE])):
@@ -131,7 +136,7 @@ def Draw():
 			add += 70
 			current += 1
 			
-			pygame.display.update()
+			if(updates): pygame.display.update()
 		
 		text = font.render(str(item), 1, white)
 		textpos.x = array[item][LOCATION][x]+10
@@ -141,12 +146,28 @@ def Draw():
 #Main loop
 while True:
 	for event in pygame.event.get():
-		#if event.type == pygame.QUIT:
-			#exit()
+		if event.type == pygame.QUIT:
+			exit()
 		if event.type == KEYDOWN:
 			if event.key == K_q:
-				exit() 
+				exit()
+			if event.key == K_u:
+				if(updates): updates = False
+				else: updates = True
+
+	Draw()
+
+	p = pygame.mouse.get_pos()
+	
+	for position in range(len(array)):
+		if p[0] > array[position][LOCATION][0] and p[0] < (array[position][LOCATION][0] + 70*len(array[position][TYPE])):
+			if p[1] > array[position][LOCATION][1] and p[1] < (array[position][LOCATION][1] + 100):
+				pygame.draw.rect(screen, white, (array[position][LOCATION][x],array[position][LOCATION][y],70*len(array[position][TYPE]),100), 5)
+
+				text = font.render(str(getInfo(position)), 1, white)
+				textpos.x = 5
+				textpos.y = Height-25
+				screen.blit(text, textpos)
+
 
 	pygame.display.update()
-	Draw()
-	time.sleep(1)
