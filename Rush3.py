d0efdd572b132124a545b91f574ff3402aa49892
ralfisalhon <ralfi.salhon@ggl.hisarschool.k12.tr>
2017-04-30 13:01:54 -0400

    Initial v
    First upload

--- Rush3.py
+++ Rush3.py
@@ -3,15 +3,15 @@ import time
 from pygame.locals import *
 from random import randrange
 pygame.init()
-pygame.display.set_caption("Rush3") #Blitz
+pygame.display.set_caption("Blitz")
 
 ##########
 updates = False
 ##########
 
-castle =  pygame.image.load("castle.png")
-coin =  pygame.image.load("coin.png")
-sword =  pygame.image.load("sword.png")
+castle 	=  pygame.image.load("castle.png")
+coin 	=  pygame.image.load("coin.png")
+sword 	=  pygame.image.load("sword.png")
 
 #KEY:		R 		G 		B
 red = 		(255,	0,		0)
@@ -26,29 +26,30 @@ yellow = 	(255,	255,	0)
 brown = 	(140,	70,		20)
 orange = 	(255,	165,	0)
 
-Width = 955
-Height = 640
-screen = pygame.display.set_mode((Width,Height))
+Width = 1200
+Height = 700
+screen = pygame.display.set_mode((Width,Height)) #,pygame.FULLSCREEN
 back = pygame.Surface((Width,Height))
 background = back.convert()
 background.fill(orange)
 screen.blit(background,(0,0))
 
 font = pygame.font.Font(None, 18)
+font2 = pygame.font.Font(None, 64)
 text = font.render("", 1, black)
 textpos = text.get_rect()
 
 array = []
 
-#INDEX KEY
-OWNER = 0
-NUM = 1
-TYPE = 2
-CARDS = 3
-ADJACENT = 4
-LOCATION = 5
-COLOUR = 6
-x,y = 0,1
+#INDEX 		KEY
+OWNER = 	0
+NUM = 		1
+TYPE = 		2
+CARDS = 	3
+ADJACENT = 	4
+LOCATION = 	5
+COLOUR = 	6
+x,y = 		0,1
 
 turnOwner = "BLUE"
 
@@ -71,6 +72,37 @@ array.append([	"NONE", 	14, "ACT", 	[0, 0, 0], 	[9, 10, 17], 		(530,440), 	brown
 array.append([	"NONE", 	15, "AT", 	[0, 0], 	[11, 16], 			(760,140), 	pink	])
 array.append([	"RED", 		16, "ACT", 	[0, 0, 0], 	[12, 13, 15, 17], 	(725,260), 	pink	])
 array.append([	"NONE", 	17, "AT", 	[0, 0], 	[14, 16], 			(760,380), 	pink	])
+
+deck = [1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 6, 6, 6, 6, 7, 7, 7, 7, 8, 8, 8, 8, 9, 9, 9, 9, 10, 10, 10, 10, "J", "J", "Q", "Q", "K", "K"]
+
+#KEY:				CARDVALUES			LOCATION						COLOUR
+holdingCards = [	[0, 0, 0, 0, 0], 	(Width - 370,Height - 140), 	blue	]
+
+for card in range(len(holdingCards[0])):
+	random = randrange(0, len(deck))
+	holdingCards[0][card] = deck[random]
+	del deck[random]
+
+print deck
+
+def PrintCards():
+	add = 0
+	for card in range(len(holdingCards[0])):
+		pygame.draw.rect(screen, holdingCards[2], (holdingCards[1][x]+add,holdingCards[1][y],70,100), 0)
+		pygame.draw.rect(screen, black, (holdingCards[1][x]+add,holdingCards[1][y],70,100), 3)
+		
+		text = font2.render(str(holdingCards[0][card]), 1, white)
+		textpos.x = holdingCards[1][x]+add+10
+		textpos.y = holdingCards[1][y]+10
+		screen.blit(text, textpos)
+
+		if(updates): pygame.display.update()
+		
+		add += 70
+
+def highlightCard(number):
+	pygame.draw.rect(screen, white, (holdingCards[1][x]+70*number,holdingCards[1][y],70,100), 5)
+
 
 #Prints the information for group 'number'
 def getInfo(number):
@@ -103,7 +135,6 @@ def DrawLines():
 			if(updates): pygame.display.update()
 
 def DrawAdjacent(position):
-	print position
 	for adjacent in range(len(array[position][ADJACENT])):
 		current = array[position][ADJACENT][adjacent]
 		pygame.draw.rect(screen, yellow, (array[current][LOCATION][x],array[current][LOCATION][y],70*len(array[current][TYPE]),100), 5)
@@ -139,10 +170,12 @@ def Draw():
 			
 			if(updates): pygame.display.update()
 		
-		text = font.render(str(item), 1, white)
+		text = font.render(str(array[item][NUM]), 1, white)
 		textpos.x = array[item][LOCATION][x]+10
 		textpos.y = array[item][LOCATION][y]+10
 		screen.blit(text, textpos)
+
+	PrintCards()
 
 #Main loop
 while True:
@@ -162,8 +195,8 @@ while True:
 	#Calculate which card is hovered over.
 	p = pygame.mouse.get_pos()
 	for position in range(len(array)):
-		if p[0] > array[position][LOCATION][0] and p[0] < (array[position][LOCATION][0] + 70*len(array[position][TYPE])):
-			if p[1] > array[position][LOCATION][1] and p[1] < (array[position][LOCATION][1] + 100):
+		if p[x] > array[position][LOCATION][x] and p[x] < (array[position][LOCATION][x] + 70*len(array[position][TYPE])):
+			if p[y] > array[position][LOCATION][y] and p[y] < (array[position][LOCATION][y] + 100):
 				#Draws white border over card when hovered over.
 				pygame.draw.rect(screen, white, (array[position][LOCATION][x],array[position][LOCATION][y],70*len(array[position][TYPE]),100), 5)
 
@@ -174,6 +207,10 @@ while True:
 				screen.blit(text, textpos)
 
 				DrawAdjacent(position)
+		if p[x] > holdingCards[1][x] and p[x] < (holdingCards[1][x] + 70*len(holdingCards[0])):
+			if p[y] > holdingCards[1][y] and p[y] < (holdingCards[1][y] + 100):
+				highlightCard((p[x]-holdingCards[1][x])/70)
 
 	#Update screen for user.
 	pygame.display.update()
+	if(updates): time.sleep(1)
